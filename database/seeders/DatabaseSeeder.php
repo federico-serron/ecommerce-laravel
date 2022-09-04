d2150551bc61b222e14fd858272ae3429610fdb7
Federico Serron Umpierrez <f.serron.umpierrez@gmail.com>
2022-09-04 12:36:10 -0300

    First commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Subcategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Storage::deleteDirectory('public/categories');
+        Storage::deleteDirectory('public/subcategories');
+
+        Storage::makeDirectory('public/categories');
+        Storage::makeDirectory('public/subcategories');
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SubcategorySeeder::class);
+    }
+}
